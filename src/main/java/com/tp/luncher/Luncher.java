b6455903a403435afb1c87998fffcd30e86fb66e
yirou <boyirou@gmail.com>
2015-10-13 14:49:16 +0200

    ...

--- src/main/java/com/tp/luncher/Luncher.java
+++ src/main/java/com/tp/luncher/Luncher.java
@@ -5,9 +5,6 @@
  */
 package com.tp.luncher;
 
-import com.tp.nasvirtuel.users.Chercheur;
-import com.tp.nasvirtuel.users.Etudiant;
-import com.tp.nasvirtuel.users.Membre;
 import com.tp.nasvirtuel.vues.UdsView;
 
 /**
@@ -15,11 +12,36 @@ import com.tp.nasvirtuel.vues.UdsView;
  * @author yirou
  */
 public class Luncher {
-//    Membre yirou=new Etudiant("Yirou");
-//    Membre lucas =new Etudiant("Lucas");
-//    Membre christine=new Chercheur("Christine");
-//    static UdsView view=new UdsView();
-//    public static void main(String[] args) {
-//        view.setVisible(true);
-//    }
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(UdsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(UdsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(UdsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(UdsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new UdsView().setVisible(true);
+            }
+        });
+    }
 }
